df1a8dc48c1c947be34326d6911e049af8a6470d
alltrue <10217099+alltrue6@user.noreply.gitee.com>
2022-06-02 13:17:54 +0800

    密码

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,7 @@
 <script lang='ts' setup>
 import { ref } from 'vue'
 import { HomeService } from "../api/index";
+import { message } from 'ant-design-vue';
 const user = ref();
 const password = ref();
 const Login = async () => {
@@ -9,13 +10,18 @@ const Login = async () => {
         user: user.value
     }
     const res = await HomeService.Login(data)
+    if (res.status == 200) {
+        message.info('登录成功');
+    } else {
+        message.error('登陆失败');
+    }
 }
 
 </script>
 
 <template>
-    <a-input v-model:value="user" placeholder="Basic usage" />
-    <a-input v-model:value="password" placeholder="Basic usage" />
+    <a-input v-model:value="user" placeholder="请输入账号" />
+    <a-input-password v-model:value="password" placeholder="请输入密码" />
     <a-button type="primary" ghost @click="Login">登录</a-button>
 </template>
 

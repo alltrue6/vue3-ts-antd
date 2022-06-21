d495481978bdcef268df87ef1c91ca0f1aa536a9
alltrue <10217099+alltrue6@user.noreply.gitee.com>
2022-06-21 09:03:01 +0800

    app

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,30 @@
 <script setup lang="ts">
 </script>
 <style>
+#app {
+  font-family: 微软雅黑, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 
+::-webkit-scrollbar-track {
+  background: hsla(0, 0%, 100%, 0.15);
+  border-radius: 3px;
+  box-shadow: inset 0 0 5px rgb(37 37 37 / 5%);
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgba(0, 0, 0, 0.12);
+  border-radius: 3px;
+  box-shadow: inset 0 0 5px rgb(0 21 41 / 5%);
+}
+
+::-webkit-scrollbar {
+  width: 6px;
+  height: 6px;
+}
+
+p {
+  margin: 0 !important;
+}
 </style>
